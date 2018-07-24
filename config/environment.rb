75e248db89dc0d7034e3ff8a12ee7cc88eaf7fbc
Adam Urbina <adamurbina@gmail.com>
2018-07-24 12:28:29 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'sqlite3'
 require 'pry'
 
 require_relative "../lib/student.rb"
-require_relative "../lib/rando.rb"
+#require_relative "../lib/rando.rb"
 
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
